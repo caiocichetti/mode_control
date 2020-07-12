d2c3f552392177cdeeaea08360d1a3f9aff3bca6
Caio Cichetti <caio_cichetti@outlook.com>
2020-07-12 14:55:47 -0300

    variables and constructor

---
+++ mode_control/Sports.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mode_control
+{
+    public class Sports
+    {
+        // Variables
+        private string mode;
+        private int maximum;
+        private int minimal;
+        private bool mixed;
+
+        // Constructor
+        public Sports(string mo, int mi, int ma, bool mx)
+        {
+            mode = mo;
+            minimal = mi;
+            maximum = ma;
+            mixed = mx;
+        }
+    }
+}
