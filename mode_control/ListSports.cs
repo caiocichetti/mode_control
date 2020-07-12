81d69da67820a76aa56e820b17347e3b76d051ea
Caio Cichetti <caio_cichetti@outlook.com>
2020-07-12 15:42:27 -0300

    query and remove

--- mode_control/ListSports.cs
+++ mode_control/ListSports.cs
@@ -25,5 +25,45 @@ namespace mode_control
         {
             save();
         }
+
+        // Consult data
+        public Sports Query(string mode)
+        {
+            Sports item = null;
+
+            foreach (Sports data in this)
+            {
+                if (data.Mode == mode)
+                {
+                    item = data;
+                }
+            }
+            return item;
+        }
+
+        public List<Sports> Queryl(bool mix)
+        {
+            List<Sports> item = new List<Sports>();
+            foreach (Sports data in this)
+            {
+                if (data.Mixed)
+                {
+                    item.Add(data);
+                }
+            }
+
+            return item;
+        }
+
+        // Delete data
+        public void Delete(Sports obj)
+        {
+            Sports item = Query(obj.Mode);
+
+            if (item != null)
+            {
+                base.Remove(item);
+            }
+        }
     }
 }
